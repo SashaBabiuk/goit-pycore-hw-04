361a35db28d88050a2034057bcd17f55516ac8c1
Oleksandr Babiuk <sashasbabuk82@gmail.com>
2026-07-30 15:13:34 +0300

    added except

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,19 @@
 def total_salary(path):
-    with open(path, "r", encoding="utf-8") as fsalary:
-        content = fsalary.readlines()
-        total = sum(float(line.split(",")[1]) for line in content)
+    try:
+        with open(path, "r", encoding="utf-8") as fsalary:
+            content = fsalary.readlines()
+            total = sum(float(line.split(",")[1]) for line in content)
 
-    return {
-        "total_salary": total,
-        "average_salary": total / len(content)
-    }
+        return {
+            "total_salary": total,
+            "average_salary": total / len(content)
+        }
+
+    except FileNotFoundError:
+        print(f"Файл '{path}' не знайдено.")
+        return {
+            "total_salary": 0,
+            "average_salary": 0
+        }
 
 print(total_salary("salary.txt"))
